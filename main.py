76c8bd6426048a2bd332cb2a5ae09a5c789662ba
giantsorbiting <mikerow4@gmail.com>
2025-01-08 18:49:59 -0500

    added character count- need to visualize

--- main.py
+++ main.py
@@ -3,12 +3,24 @@ def main():
         file_contents = f.read()
     total_words = count_words(file_contents)
     longest_word = find_longest(file_contents)
+    character_count = count_characters(file_contents)
     print(f"Total words: {total_words}")
+    print(f"Character count: {character_count}")
     print(f"Longest word: '{longest_word}' with {len(longest_word)} characters")
 
 def count_words(file_contents):
     words = file_contents.split()
     return len(words)
+
+def count_characters(file_contents):
+    my_dict = {}
+    lower_contents = file_contents.lower()
+    for character in lower_contents:
+        if character in my_dict:
+            my_dict[character] += 1
+        else:
+            my_dict[character] = 1
+    return my_dict
 
 def find_longest(file_contents):
     longest_word = ""
